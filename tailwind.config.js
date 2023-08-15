4ad814bd2e4b422e1f8941ecb53736c6ac773f94
02prachi2002 <211288@juitsolan.in>
2023-08-15 08:59:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ggsans': ['ggsans-normal'],
+        'ginto':['Angkor', 'cursive'],
+        
+        
+      },
+      spacing: {
+        '30' : '120px'
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
